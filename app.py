9da9b49919d656c2af1808dc660274a35d21d7d1
Pavgro <pavelgrobov@gmail.com>
2022-03-21 17:53:43 +0200

    add create article page

--- app.py
+++ app.py
@@ -1,20 +1,24 @@
 from flask import Flask, render_template, url_for
 from flask_sqlalchemy import SQLAlchemy
 from datetime import datetime
+
+
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite://blog.db'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///blog.db'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
 
 
-class Article(db.model):
+class Article(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     title = db.Column(db.String(100), nullable=False)
-    intro = db.Column(db.String(300), nullbale=False)
+    intro = db.Column(db.String(300), nullable=False)
     text = db.Column(db.Text, nullable=False)
     date = db.Column(db.DateTime, default=datetime.utcnow())
 
     def __repr__(self):
         return '<Article %r>' % self.id
+
 
 @app.route('/home')
 @app.route('/')
@@ -26,9 +30,11 @@ def index():
 def about():
     return render_template("about.html")
 
-@app.route('/user/<string:name>/<int:id>')
-def user(name, id):
-    return "User page" + name + str(id)
+
+@app.route('/create-article')
+def create_article():
+    return render_template("create-article.html")
+
 
 
 if __name__ == "__main__":
